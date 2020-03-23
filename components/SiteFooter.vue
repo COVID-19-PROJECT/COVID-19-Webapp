39722bbb862515d7258ec591688f2bd8fb17bed8
Javier Toledo <javiertoledos@gmail.com>
2020-03-22 18:17:23 -0600

    Add empty  terms of use page

---
+++ components/SiteFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <nuxt-link :to="localePath('/terms-of-use')">
+        {{ $t('navigation.terms-of-use') }}
+      </nuxt-link>
+    </div>
+  </footer>
+</template>
---
+++ pages/terms-of-use.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container">
+        <h1 class="title is-1">{{ $t('terms-of-use.title') }}</h1>
+    </div>
+</template>
