315e9de4e3cbbb9060a832f263418ae4c8ba0611
Javier Toledo <javiertoledos@gmail.com>
2020-03-22 22:02:30 -0600

    Add initial participate page

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,26 +1,43 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <nuxt-link class="navbar-item" :to="localePath('/')">
-        <logo class="navbar-logo" />
-        <span class="title is-4">{{ $t('general.site-name') }}</span>
-      </nuxt-link>
-      <a
-        role="button"
-        class="navbar-burger"
-        :class="{'is-active': isActive}"
-        aria-label="menu"
-        aria-expanded="false"
-        @click="toggleMenu"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+    <div class="container">
+      <div class="navbar-brand">
+        <nuxt-link class="navbar-item" :to="localePath('/')">
+          <logo class="navbar-logo" />
+          <span class="title is-4">{{ $t('general.site-name') }}</span>
+        </nuxt-link>
+        <a
+          role="button"
+          class="navbar-burger"
+          :class="{'is-active': isActive}"
+          aria-label="menu"
+          aria-expanded="false"
+          @click="toggleMenu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-    <div class="navbar-menu" :class="{'is-active': isActive}">
-      <slot />
+      <div class="navbar-menu" :class="{'is-active': isActive}">
+        <nuxt-link :to="localePath('/participate')" class="navbar-item">
+          {{ $t('navigation.participate') }}
+        </nuxt-link>
+        <nuxt-link :to="localePath('/terms-of-use')" class="navbar-item">
+          {{ $t('navigation.terms-of-use') }}
+        </nuxt-link>
+      </div>
+
+      <div class="navbar-end is-hidden-mobile is-hidden-tablet-only">
+        <div class="navbar-item">
+          <div class="buttons">
+            <nuxt-link :to="localePath('/participate')" class="button is-primary">
+              {{ $t('navigation.participate') }}
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
@@ -40,6 +57,11 @@ export default {
   methods: {
     toggleMenu () {
       this.isActive = !this.isActive
+    },
+  },
+  watch: {
+    '$route' () {
+      this.isActive = false
     },
   },
 }
