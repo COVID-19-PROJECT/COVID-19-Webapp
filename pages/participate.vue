6586e118ffa010de828669bb79c42ddc70262c07
Javier Toledo <javiertoledos@gmail.com>
2020-03-23 20:55:40 -0600

    Improve styling and add google forms

--- pages/participate.vue
+++ pages/participate.vue
@@ -1,80 +1,50 @@
 <template>
-<div>
-  <div class="first-widget parallax" id="portfolio">
-		<div class="parallax-overlay">
-			<div class="container pageTitle">
-				<div class="row">
-					<div class="col-md-6 col-sm-6">
-						<h2 class="page-title">{{ $t('participate.title') }}</h2>
-					</div> <!-- /.col-md-6 -->
-				</div> <!-- /.row -->
-			</div> <!-- /.container -->
-		</div> <!-- /.parallax-overlay -->
-	</div> <!-- /.pageTitle -->
-	<div class="container">	
-		<div class="row">
-
-			<div class="col-md-8 blog-posts">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="contact-wrapper">
-    <div v-md="$t('participate.intro-text')" class="subtitle is-size-3" />
-
-						</div> <!-- /.contact-wrapper -->
-					</div> <!-- /.col-md-12 -->
-				</div> <!-- /.row -->
-				<div class="row">
-                    <div class="col-md-12">
-                        <div class="contact-form">
-                            <h3>Send a Direct Message</h3>
-	                        <div class="widget-inner">
-                            <form action="#" method="post" id="contact-form">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <p>
-                                            <label for="name">Your Name:</label>
-                                            <input type="text" name="name" id="name">
-                                        </p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <p>
-                                            <label for="email">Email Address:</label>
-                                             <input type="text" name="email" id="email">
-                                        </p>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <p>
-                                            <label for="subject">Subject:</label>
-                                             <input type="text" name="subject" id="subject">
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <p>
-                                            <label for="message">Your message:</label>
-                                            <textarea name="message" id="message"></textarea>
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <button class="mainBtn" id="submit">Send Message</button>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                	<div class="col-md-12">
-                                		<div id="result"></div>
-                                	</div> <!-- /.col-md-12 -->
-                                </div> <!-- /.row -->
-                            </form>
-                          </div> <!-- /.widget-inner -->
-                        </div> <!-- /.contact-form -->
-                    </div> <!-- /.col-md-12 -->
-                </div> <!-- /.row -->
-			</div> <!-- /.col-md-8 -->
-
-		</div> <!-- /.row -->
-	</div> <!-- /.container -->	
+  <div>
+    <div id="portfolio" class="first-widget parallax">
+      <div class="parallax-overlay">
+        <div class="container pageTitle">
+          <div class="row justify-content-md-center">
+            <div class="col-md-6 col-sm-6">
+              <h2 class="page-title text-center">
+                {{ $t('participate.title') }}
+              </h2>
+            </div> <!-- /.col-md-6 -->
+          </div> <!-- /.row -->
+        </div> <!-- /.container -->
+      </div> <!-- /.parallax-overlay -->
+    </div> <!-- /.pageTitle -->
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-12">
+              <div v-md="$t('participate.intro-text')" class="pt-3" />
+              <div class="contact-form">
+                <iframe
+                  :src="$t('participate.form-url')"
+                  width="460"
+                  height="1350"
+                  frameborder="0"
+                  marginheight="0"
+                  marginwidth="0"
+                >
+                  {{ $t('participate.loading') }}
+                </iframe>
+              </div> <!-- /.contact-form -->
+            </div> <!-- /.col-md-12 -->
+          </div> <!-- /.row -->
+        </div> <!-- /.col-md-8 -->
+      </div> <!-- /.row -->
+    </div> <!-- /.container -->
   </div>
 </template>
+
+<style lang="scss">
+.contact-form {
+  text-align: center;
+
+  iframe {
+    margin: 0 auto;
+  }
+}
+</style>
