c06a06d704a4627f512807fcf3f9da6455d5336a
Javier Toledo <javiertoledos@gmail.com>
2020-03-23 20:59:41 -0600

    Reduce row nesting in participate page

--- pages/participate.vue
+++ pages/participate.vue
@@ -16,23 +16,17 @@
     <div class="container">
       <div class="row justify-content-md-center">
         <div class="col-md-8">
-          <div class="row">
-            <div class="col-md-12">
-              <div v-md="$t('participate.intro-text')" class="pt-3" />
-              <div class="contact-form">
-                <iframe
-                  :src="$t('participate.form-url')"
-                  width="460"
-                  height="1350"
-                  frameborder="0"
-                  marginheight="0"
-                  marginwidth="0"
-                >
-                  {{ $t('participate.loading') }}
-                </iframe>
-              </div> <!-- /.contact-form -->
-            </div> <!-- /.col-md-12 -->
-          </div> <!-- /.row -->
+          <div v-md="$t('participate.intro-text')" class="pt-3" />
+          <div class="contact-form">
+            <iframe
+              :src="$t('participate.form-url')"
+              width="460"
+              height="1350"
+              frameborder="0"
+            >
+              {{ $t('participate.loading') }}
+            </iframe>
+          </div> <!-- /.contact-form -->
         </div> <!-- /.col-md-8 -->
       </div> <!-- /.row -->
     </div> <!-- /.container -->
