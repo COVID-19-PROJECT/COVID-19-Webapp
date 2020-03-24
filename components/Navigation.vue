fb6c722c314e6bb64d36f736da79f2d37016d0ac
Javier Toledo <javiertoledos@gmail.com>
2020-03-23 21:12:16 -0600

    Improve styling and add google forms (#5)
    * Improve styling and add google forms
    
    * Reduce row nesting in participate page

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,41 +1,35 @@
 <template>
   <header class="site-header clearfix">
-		<div class="container">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="float-left logo">
+            <nuxt-link class="navbar-item" :to="localePath('/')">
+              <logo class="navbar-logo" />
+            </nuxt-link>
+          </div> <!-- /.logo -->
+          <div class="main-navigation float-right">
+            <nav class="main-nav visible-md visible-lg">
+              <ul class="sf-menu">
+                <li>
+                  <nuxt-link :to="localePath('/participate')" class="navbar-item is-hidden-desktop">
+                    {{ $t('navigation.participate') }}
+                  </nuxt-link>
+                </li>
+              </ul> <!-- /.sf-menu -->
+            </nav> <!-- /.main-nav -->
 
-			<div class="row">
-
-				<div class="col">
-					<div class="float-left logo">
-        <nuxt-link class="navbar-item" :to="localePath('/')">
-          <logo class="navbar-logo" />
-        </nuxt-link>
-					</div>	<!-- /.logo -->
-
-					<div class="main-navigation float-right">
-
-						<nav class="main-nav visible-md visible-lg">
-							<ul class="sf-menu" >
-					            <li>        <nuxt-link :to="localePath('/participate')" class="navbar-item is-hidden-desktop">
-          {{ $t('navigation.participate') }}
-        </nuxt-link></li>
-							</ul> <!-- /.sf-menu -->
-						</nav> <!-- /.main-nav -->
-
-						<!-- This one in here is responsive menu for tablet and mobiles -->
-					    <div class="responsive-navigation visible-sm visible-xs">
-					        <a href="#nogo" class="menu-toggle-btn">
-					            <i class="fa fa-bars"></i>
-					        </a>
-					    </div> <!-- /responsive_navigation -->
-
-					</div> <!-- /.main-navigation -->
-
-				</div> <!-- /.col-md-12 -->
-
-			</div> <!-- /.row -->
-
-		</div> <!-- /.container -->
-	</header> <!-- /.site-header -->
+            <!-- This one in here is responsive menu for tablet and mobiles -->
+            <div class="responsive-navigation visible-sm visible-xs">
+              <a href="#nogo" class="menu-toggle-btn">
+                <i class="fa fa-bars" />
+              </a>
+            </div> <!-- /responsive_navigation -->
+          </div> <!-- /.main-navigation -->
+        </div> <!-- /.col-md-12 -->
+      </div> <!-- /.row -->
+    </div> <!-- /.container -->
+  </header> <!-- /.site-header -->
 </template>
 
 <script>
@@ -50,14 +44,14 @@ export default {
       isActive: false,
     }
   },
-  methods: {
-    toggleMenu () {
-      this.isActive = !this.isActive
-    },
-  },
   watch: {
     '$route' () {
       this.isActive = false
+    },
+  },
+  methods: {
+    toggleMenu () {
+      this.isActive = !this.isActive
     },
   },
 }
